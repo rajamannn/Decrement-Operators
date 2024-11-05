c0bc13a030e96da979f5755a82dde2226148a5e7
Aman Raj <160530800+rajamannn@users.noreply.github.com>
2024-11-06 00:29:24 +0530

    Add files via upload

---
+++ Decreament Opreators.cpp
@@ -0,0 +1,13 @@
+//Program to print the decrement operator in C++
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  int var = 10;
+    cout << "Value before decrement: " << var << endl;
+  var--;
+    cout << "Value after decrement: " << var;
+    return 0;
+}
